13e17e67632f9635a3740c83fbab88f8f48ba7d0
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-09-03 11:50:03 +0700

    Initial commit

---
+++ lib/App/TextTableUtils.pm
@@ -0,0 +1,21 @@
+package App::TextTableUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: CLI utilities related to text tables
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities related to
+text tables:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::texttable>
+
+L<App::TableDataUtils>
